cb7a52839e113dbac9d105178696161e2cca74c9
Rafael Ruiz-Clavijo <rruizclavijo@miuandes.cl>
2020-09-25 11:10:06 -0300

    Programa Assembly modificado

--- Programas_Assembly.py
+++ Programas_Assembly.py
@@ -8,27 +8,23 @@ Created on Thu Sep 24 18:13:06 2020
 #ASSEMBLYS
 
 #Contar pares
-inicio = 4 #DEFINICION DE VARIABLES
+inicio = 0 #DEFINICION DE VARIABLES
 fin = 7  
 pares = 0  #REGISTRO A
 mem = [2,5,6,4,3,1,5,7,8,9,9] #MEMORIA
 
 while inicio < fin:  #2
-    while mem[inicio] > 1: #2
-        mem[inicio] -= 2 #1
+    mem[inicio] = mem[inicio] & 1 #1
     if mem[inicio] == 0: #2
-        pares+= 1; #1
-    inicio+=1; #1
-A = pares #1
+        pares+= 1 #1
+    inicio+=1 #1
 
 print(pares)
 
 """
 MOV INICIO,4
 MOV FIN,8
-SUB (INICIO),2
-CMP (INICIO),1
-JLE 1
+AND (INICIO),1
 CMP (INICIO),0
 JNE 7
 ADD A,1
